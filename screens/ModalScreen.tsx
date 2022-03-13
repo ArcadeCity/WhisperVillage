5caa84beae20e4225a7c2e801789c3d27f336afa
Christopher David <chris@arcade.city>
2022-03-13 15:37:53 -0500

    edit modal

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Linking, Platform, Pressable, StyleSheet } from 'react-native'
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import EditScreenInfo from '../components/EditScreenInfo'
+import { Text, View } from '../components/Themed'
 
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/ModalScreen.tsx" />
+      <Text style={{ marginTop: 20, fontSize: 16, fontWeight: 'bold' }}>Bitcoin Testnet</Text>
+      <Text>Don't use real coins</Text>
+
+      <Pressable
+        style={{ marginVertical: 20 }}
+        onPress={() => Linking.openURL('https://github.com/ArcadeCity/whispervillage')}
+      >
+        <Text>Code & explainer: </Text>
+
+        <Text style={{ color: 'darkblue' }}>https://github.com/ArcadeCity/WhisperVillage</Text>
+      </Pressable>
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -32,4 +40,4 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
-});
+})
